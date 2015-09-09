7ff1ffd2764f8c8efefc7814541a90022938c5c8
przemek29 <przemyslawwojcik91@gmail.com>
2015-09-09 09:30:30 +0100

    Brush-up

---
+++ Chapter 10/10_13/10_13.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+#include "person.h"
+
+void presentation(person);
+
+int main()
+{
+    person composer, author;
+
+    composer.remember("Fryderyk Chopin", 36);
+    author.remember("Marcel Proust", 34);
+
+    presentation(composer);
+    presentation(author);
+}
+
+void presentation(person someone)
+{
+    cout << "I have the honor to present you, \nHere in person: ";
+    someone.print();
+}
